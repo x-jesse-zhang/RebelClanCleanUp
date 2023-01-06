0b782995ecc2360f6a4b58832c21f097dfb0b8f2
Jesse Zhang <macisasandwich@gmail.com>
2023-01-05 17:52:47 -0800

    Add project files.

---
+++ RebelCleanupBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Library;
+
+namespace RebelClanCleanUp
+{
+    internal class RebelCleanupBehavior : CampaignBehaviorBase
+    {
+        public override void RegisterEvents()
+        {
+            CampaignEvents.OnSettlementOwnerChangedEvent.AddNonSerializedListener(
+                this, 
+                new Action<
+                    Settlement, 
+                    bool, 
+                    Hero, 
+                    Hero, 
+                    Hero, 
+                    ChangeOwnerOfSettlementAction.ChangeOwnerOfSettlementDetail
+                >(this.OnSettlementOwnerChanged)
+            );
+        }
+
+        private void OnSettlementOwnerChanged(
+            Settlement settlement, 
+            bool openToClaim, 
+            Hero newOwner, 
+            Hero oldOwner, 
+            Hero capturerHero, 
+            ChangeOwnerOfSettlementAction.ChangeOwnerOfSettlementDetail detail
+        ){
+            if (oldOwner.IsRebel) {
+                DestroyClanAction.ApplyByFailedRebellion(oldOwner.Clan);
+                InformationMessage msg = new InformationMessage(
+                                    oldOwner.Clan.GetName() + " has been destroyed for failing their rebellion!"
+                                );
+                InformationManager.DisplayMessage(msg);
+            }
+        }
+
+        public override void SyncData(IDataStore dataStore) {}
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,18 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace RebelClanCleanUp
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter gameInitializer = (CampaignGameStarter)gameStarterObject;
+                gameInitializer.AddBehavior(new RebelCleanupBehavior());
+            }
+        }
+    }
+}
